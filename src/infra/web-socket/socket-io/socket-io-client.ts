fde9fa41aa9e12ab9d8bb631f1e2aaea7081378b
rene-0 <rene.colombaro@hotmail.com>
2022-12-06 18:34:15 -0300

    chore: use localhost instead of ip

--- src/infra/web-socket/socket-io/socket-io-client.ts
+++ src/infra/web-socket/socket-io/socket-io-client.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-export const socketClient = io('http://192.168.0.132:5050', { autoConnect: false })
+export const socketClient = io('http://localhost:5050', { autoConnect: false })
 const accessToken = localStorage.getItem('accessToken')
 if (accessToken) {
   socketClient.auth = { accessToken: { token: accessToken } }
